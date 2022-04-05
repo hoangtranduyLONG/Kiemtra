ffe793e9e7c94d885c23aa81a800f1c8b66dddbb
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-04-05 22:10:24 +0700

    ghi chú

--- src/B1.java
+++
@@ -1,9 +0,0 @@
-public class B1 {
-    public static void main(String[] args) {
-        int[] c = {1, 2, 3, 4, 5};
-        int[] n = new int[10];
-        for (int i = 0; i < 10; i++) {
-            n[i] = i;
-        }
-    }
-    }
