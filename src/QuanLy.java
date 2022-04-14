ff9c3e662a1270402cac1abb2b0ddc884015939d
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-04-14 16:28:56 +0700

    ghi chú

--- src/QuanLy.java
+++ src/QuanLy.java
@@ -2,6 +2,6 @@ public interface QuanLy <T>{
     void them(T t);
     void sua(String Name,T t);
     void xoa(String Name);
-    T timKiem(String Name);
+    int timKiem(String Name);
     void print();
 }
--- src/QuanLySinhVien.java
+++ src/QuanLySinhVien.java
@@ -1,10 +1,11 @@
-public class QuanLySinhVien implements QuanLy<SinhVien>{
-private SinhVien[]danhSach =new SinhVien[10];
-private int ngay=0;
+public class QuanLySinhVien implements QuanLy<SinhVien> {
+    private SinhVien[] danhSach = new SinhVien[10];
+    private int ngay = 0;
+
     @Override
     public void them(SinhVien sinhVien) {
-danhSach[ngay]= sinhVien;
-ngay++;
+        danhSach[ngay] = sinhVien;
+        ngay++;
     }
 
     @Override
@@ -18,20 +19,26 @@ ngay++;
     }
 
     @Override
-    public SinhVien timKiem(String Name) {
-        return null;
+    public int timKiem(String Name) {
+        for (int i = 0; i < ngay; i++) {
+            if (Name == danhSach[i].getTen()) {
+                return i;
+            }
+        }
+        return -1;
     }
+
 
     @Override
     public void print() {
-for(int i=0;i<ngay;i++){
-    System.out.println(danhSach[i]);
-}
+        for (int i = 0; i < ngay; i++) {
+            System.out.println(danhSach[i]);
+        }
     }
 
     public static void main(String[] args) {
-        QuanLySinhVien ql=new QuanLySinhVien();
-        SinhVien sv1= new SinhVien("SV20035","Duong",20,8);
+        QuanLySinhVien ql = new QuanLySinhVien();
+        SinhVien sv1 = new SinhVien("SV20035", "Duong", 20, 8);
         ql.them(sv1);
         ql.print();
     }
